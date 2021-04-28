a206afe1d036317aec6796c413fe0aa49dae039f
Carlos Vallejos <cvallejosmenares@gmail.com>
2021-04-27 22:18:01 -0400

    fix path¿

--- php-fpm/files/startup.sh
+++ php-fpm/files/startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 role=${CONTAINER_ROLE:-web}
-if [ -d "/opt/develop" ]; then
+if [ -d "/var/www" ]; then
 	if [ -z $PRODUCCION ]; then
-    	cd /opt/develop
+    	cd /var/www
     	composer install
 	fi
 fi
-touch /opt/develop/storage/logs/laravel.log
+touch /var/www/storage/logs/laravel.log
 
 case "$role" in
     web)
@@ -14,7 +14,7 @@ case "$role" in
         php-fpm7.2 -y /etc/php/7.2/fpm/php-fpm.conf -R
     ;;
     worker)
-        php /opt/develop/artisan queue:work --verbose
+        php /var/www/artisan queue:work --verbose
     ;;
 esac
-tail -f /opt/develop/storage/logs/laravel.log
+tail -f /var/www/storage/logs/laravel.log
